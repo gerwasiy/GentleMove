a60442277ce78a1a224e14b28bd9be140a53c4ce
gerwasiy <gerwasiy@gmail.com>
2023-11-11 11:41:19 +0200

    create template fot about page

---
+++ pages/About.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Про нас</h1>
+        <section></section>
+      </main>
+      <Footer />
+    </>
+  );
+}
